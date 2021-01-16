b9d9906abf777b9e53bd4f4d2541358d9e2499e0
minakalina <nantachaporn.r@ku.th>
2021-01-16 10:04:20 +0700

    STR modify_str(แปลงตัวอักษรม แทนที่, นับตัว), connatention(เชื่อม), format(ฟังก์ชันใส่ในปย.) , f-string(ซ้อนตัวแปร)

---
+++ string/f-strings.py
@@ -0,0 +1,5 @@
+name = "BB"
+age = 21
+result = f"my name is {name}, and I am {age}"
+print("result:", result)  # result: my name is BB, and I am 21
+# เอาในตัวแปรมาใส่ในอีกตัวแปรซ้อนๆกันเป็นประโยค เท่ๆๆๆๆๆๆ
---
+++ string/string_format.py
@@ -0,0 +1,6 @@
+age = 21
+txt = "My name is BB, and I am {}"
+result = txt.format(age)
+
+print("result:", result)  # result: My name is BB, and I am 21
+# เอาage มาใส่ในtxt โดยใช้ฟังก์ชันformat
